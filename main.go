610537dc176220dbb7301cca59776ab81446cf76
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-07 21:24:15 -0500

    add: main.go

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"strings"
+)
+
+var (
+	prometheusUrl string
+	from          string
+	to            string
+	interval      string
+	metrics       []string
+)
+
+// load the vars from environment variables if not set
+func initVars() {
+	if prometheusUrl == "" {
+		prometheusUrl = os.Getenv("PD_PROMETHEUS_URL")
+		if prometheusUrl == "" {
+			prometheusUrl = "http://localhost:9090"
+		}
+	}
+
+	if len(metrics) == 0 {
+		metricsEnv := os.Getenv("PD_METRICS")
+		if metricsEnv == "" {
+			metrics = []string{"http_requests_total"}
+		} else {
+			metrics = strings.Split(metricsEnv, ",")
+		}
+	}
+
+	if from == "" {
+		from = os.Getenv("PD_FROM")
+		if from == "" {
+			from = "2023-01-01T00:00:00Z"
+		}
+	}
+
+	if to == "" {
+		to = os.Getenv("PD_TO")
+		if to == "" {
+			to = "2023-01-01T00:00:00Z"
+		}
+	}
+
+	if interval == "" {
+		interval = os.Getenv("PD_INTERVAL")
+		if interval == "" {
+			interval = "1m"
+		}
+	}
+}
+
+func main() {
+	// define flags for the vars
+	flag.StringVar(&prometheusUrl, "prometheus-url", "", "Prometheus URL")
+	flag.StringVar(&from, "from", "", "Start time for the query")
+	flag.StringVar(&to, "to", "", "End time for the query")
+	flag.StringVar(&interval, "interval", "", "Interval for the query")
+	metricsFlag := flag.String("metrics", "", "Metrics to query")
+
+	// parse the flags
+	flag.Parse()
+
+	// split metrics flag if set
+	if *metricsFlag != "" {
+		metrics = strings.Split(*metricsFlag, ",")
+	}
+
+	// initialize variables
+	initVars()
+}
