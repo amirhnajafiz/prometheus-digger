b022801b0bc563de8e81c53e50f3045447c42199
amirhnajafiz <najafizadeh21@gmail.com>
2025-07-12 15:54:46 -0400

    remove: unused types of metrics

--- internal/configs/configs.go
+++ internal/configs/configs.go
@@ -3,8 +3,18 @@ package configs
 import (
 	"encoding/json"
 
+	"github.com/amirhnajafiz/prometheus-digger/internal/models"
 	"github.com/amirhnajafiz/prometheus-digger/pkg"
 )
+
+// Config is a module that holds the configuration of the application.
+type Config struct {
+	URL      string         `json:"url"`
+	From     string         `json:"from"`
+	To       string         `json:"to"`
+	PoolSize int            `json:"pool_size"`
+	Queries  []models.Query `json:"queries"`
+}
 
 // LoadConfigs reads the config file and returns a config instance.
 func LoadConfigs(path string) (*Config, error) {
