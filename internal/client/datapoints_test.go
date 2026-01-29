c1a9baa49b841e17836bf2b880b4735029c8e657
amirhnajafiz <najafizadeh21@gmail.com>
2026-01-29 11:50:14 -0500

    add: datapoints test

--- internal/client/datapoints_test.go
+++ internal/client/datapoints_test.go
@@ -43,3 +43,50 @@ func TestGetDataPoints(t *testing.T) {
 		}
 	}
 }
+
+func TestSplitTimeRange(t *testing.T) {
+	cases := [][]time.Time{
+		{
+			time.Now(), time.Now().Add(10 * time.Second),
+			time.Now(), time.Now().Add(10 * time.Second),
+			time.Now(), time.Now().Add(1 * time.Hour),
+			time.Now(), time.Now().Add(1 * time.Hour),
+		},
+	}
+
+	steps := []time.Duration{
+		time.Second,
+		5 * time.Second,
+		time.Second,
+		5 * time.Second,
+	}
+
+	maxPoints := []int{
+		2,
+		2,
+		20,
+		20,
+	}
+
+	want := []int{
+		7,
+		4,
+		182,
+		126,
+	}
+
+	for i := range len(cases) {
+		got := SplitTimeRange(cases[i][0], cases[i][1], steps[i], maxPoints[i])
+		if len(got) != want[i] {
+			t.Errorf(
+				"input: (%s, %s, %f, %d), output: %d, expected: %d",
+				cases[i][0].Format(time.RFC3339),
+				cases[i][1].Format(time.RFC3339),
+				steps[i].Seconds(),
+				maxPoints[i],
+				len(got),
+				want[i],
+			)
+		}
+	}
+}
